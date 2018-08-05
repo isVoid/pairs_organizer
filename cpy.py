910644e49e58dbe6d431ebcfa3dbba18ae926812
Wang Yinghao <michaelwang0905@icloud.com>
2018-08-05 16:57:59 -0500

    v2.0 alpha

--- cpy.py
+++ cpy.py
@@ -15,6 +15,15 @@
 import os
 import shutil
 import argparse
+import logging
+from logging.config import dictConfig
+import datetime
+import json
+from tqdm import tqdm
+from organizer import organize_pairs
+from const import RAWEXT
+from todng import raw2dng
+from pathlib import Path
 
 try:
     input = raw_input
@@ -24,21 +33,41 @@ except NameError:
 desc = """
 RAW file copy utility.
 """
-RAWEXT = ['ARW', 'CR2', 'NEF', 'DNG']
 
-def copy_pairs_raw_only(s, d):
+def copy_pairs_raw_only(s, d, o):
     for dir, _, files in os.walk(s):
-        for f in files:
-            EXT = f.split('.')[-1].to_upper()
+        logger.info("Copying RAW files, %d to copy." % (len(files) - o))
+        # for f in tqdm(files):
+        for i in tqdm(range(o+1, len(files))):
+            f = files[i]
+            EXT = f.split('.')[-1].upper()
             if EXT in RAWEXT:
                 shutil.copy(os.path.join(dir, f), os.path.join(d, f))
 
+def del_raw_files(d):
+    logger.debug(d)
+    for dir, _, files in os.walk(d):
+        for f in files:
+            EXT = f.split('.')[-1].upper()
+            if EXT in RAWEXT and EXT != "DNG":
+                os.remove(os.path.join(d, f))
+
 if __name__ == "__main__":
+    with open("logging.json", 'r') as logging_configuration_file:
+        config_dict = json.load(logging_configuration_file)
+        # logname = os.path.join(os.getcwd(), "log", datetime.date.today().strftime("%B-%d-%Y") + ".log")
+        # print (logname)
+        # config_dict["handlers"]["filename"] = logname
+        # print (config_dict)
+        logging.config.dictConfig(config_dict)
+
+    logger = logging.getLogger(__name__)
     # Parse args or take input
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('--source', '-s', help='Full path of source folder.')
-    parser.add_argument('--destination', '-d', help='Full path of destination folder.')
-
+    parser.add_argument('--source', '-s', type=str, help='Full path of source folder.')
+    parser.add_argument('--destination', '-d', type=str, help='Full path of destination folder.')
+    parser.add_argument('--name', '-n', type=str, help='Name of the shot, [3A]Batch###')
+    parser.add_argument('--offset', '-o', type=int, default = 0, help='Number of FILES (Including JPGS) to Offset During Copy. Sorted by name.')
     args = parser.parse_args()
     try:
         s = args.source
@@ -50,6 +79,16 @@ if __name__ == "__main__":
     except:
         d = input("Input the destination folder: ")
 
-    copy_pairs_raw_only(s, d)
+    try:
+        n = args.name
+    except:
+        n = input("Name of the shot?")
 
-    
+    o = args.offset
+
+    copy_pairs_raw_only(s, d, o)
+    organize_pairs(d, n, False, False)
+    raw2dng(os.path.join(d, "Noisy"))
+    raw2dng(os.path.join(d, "Clean"))
+    del_raw_files(os.path.join(d, "Noisy"))
+    del_raw_files(os.path.join(d, "Clean"))
--- organizer.py
+++ organizer.py
@@ -1,69 +1,92 @@
 #!/usr/bin/python
+# Copyright [2018] [Wang Yinghao]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
-
+import argparse
+import logging
 try:
     input = raw_input
 except NameError:
     pass
 
-d = input("The folder where the pairs locates, in full path: ")
-n = input("Name of the shot? ")
-dng = input("Is this a DNG folder? y/n")
-if not dng == 'y':
-    j = input("Is this a JPG folder? y/n: ")
-    if not j == "y" and not j == "n":
-        raise ValueError("Unrecognized input j: %s" % j)
-    elif j == "y":
-        j = True
-    elif j == "n":
-        j = False
-else:
-    dng = True;
-    j = False
+desc = """
+Utility to organize pairs in folder.
+"""
+logger = logging.getLogger(__name__)
 
-if not os.path.exists(os.path.join(d, "Clean")):
-    os.mkdir(os.path.join(d, "Clean"))
-if not os.path.exists(os.path.join(d, "Noisy")):
-    os.mkdir(os.path.join(d, "Noisy"))
+def organize_pairs(d, n, dng, j):
 
-CleanCount = 0
-NoisyCount = 0
+    if not os.path.exists(os.path.join(d, "Clean")):
+        os.mkdir(os.path.join(d, "Clean"))
+    if not os.path.exists(os.path.join(d, "Noisy")):
+        os.mkdir(os.path.join(d, "Noisy"))
 
-flag = 1
-# flag == 1, first frame is noisy
-# flag == 0, first frame is clean
-# Check before run.
-# Generally first frame must be noisy, if not, think why.
+    CleanCount = 0
+    NoisyCount = 0
 
-fs = []
-for f in os.listdir(d):
-    ff = os.path.join(d, f)
-    if not os.path.isdir(ff):
-        fs.append(ff)
-fs = sorted(fs)
+    flag = 1
+    # flag == 1, first frame is noisy
+    # flag == 0, first frame is clean
+    # Check before run.
+    # Generally first frame must be noisy, if not, think why.
 
-for f in fs:
-    print (f)
-    ext = f.split(".")[-1].upper()
-    if ext == "ARW" or ext == "JPG" or ext == 'DNG':
-        if (CleanCount + NoisyCount) % 2 == flag:
-            if not j:
-                if not dng:
-                    os.rename(os.path.join(d, f), os.path.join(d, "Clean", "%s_c_" % n + str(CleanCount).zfill(5) + ".ARW"))
+    fs = []
+    for f in os.listdir(d):
+        ff = os.path.join(d, f)
+        if not os.path.isdir(ff):
+            fs.append(ff)
+    fs = sorted(fs)
+
+    for f in fs:
+        logger.debug("  Organizing  " + f)
+        ext = f.split(".")[-1].upper()
+        if ext == "ARW" or ext == "JPG" or ext == 'DNG':
+            if (CleanCount + NoisyCount) % 2 == flag:
+                if not j:
+                    if not dng:
+                        os.rename(os.path.join(d, f), os.path.join(d, "Clean", str(CleanCount).zfill(5) + ".ARW"))
+                    else:
+                        os.rename(os.path.join(d, f), os.path.join(d, "Clean", str(CleanCount).zfill(5) + ".dng"))
                 else:
-                    os.rename(os.path.join(d, f), os.path.join(d, "Clean", "%s_c_" % n + str(CleanCount).zfill(5) + ".dng"))
+                    os.rename(os.path.join(d, f), os.path.join(d, "Clean", str(CleanCount).zfill(5) + ".JPG"))
+                CleanCount += 1
             else:
-                os.rename(os.path.join(d, f), os.path.join(d, "Clean", "%s_c_" % n + str(CleanCount).zfill(5) + ".JPG"))
-            CleanCount += 1
+                if not j:
+                    if not dng:
+                        os.rename(os.path.join(d, f),os.path.join(d, "Noisy", str(NoisyCount).zfill(5) + ".ARW"))
+                    else:
+                        os.rename(os.path.join(d, f),os.path.join(d, "Noisy", str(NoisyCount).zfill(5) + ".dng"))
+                else:
+                    os.rename(os.path.join(d, f), os.path.join(d, "Noisy", str(CleanCount).zfill(5) + ".JPG"))
+                NoisyCount += 1
+
+    logger.info("Sorted %d Clean imgs and %d Noisy imgs" % (CleanCount, NoisyCount))
+
+if __name__ == "__main__":
+
+    d = input("The folder where the pairs locates, in full path: ")
+    n = input("Name of the shot?")
+    dng = input("Is this a DNG folder? y/n")
+    if not dng == 'y':
+        j = input("Is this a JPG folder? y/n: ")
+        if j == "y":
+            j = True
         else:
-            if not j:
-                if not dng:
-                    os.rename(os.path.join(d, f),os.path.join(d, "Noisy", "%s_n_" % n + str(NoisyCount).zfill(5) + ".ARW"))
-                else:
-                    os.rename(os.path.join(d, f),os.path.join(d, "Noisy", "%s_n_" % n + str(NoisyCount).zfill(5) + ".dng"))
-            else:
-                os.rename(os.path.join(d, f), os.path.join(d, "Noisy", "%s_n_" % n + str(CleanCount).zfill(5) + ".JPG"))
-            NoisyCount += 1
+            j = False
+    else:
+        dng = True;
+        j = False
 
-print ("%d Clean imgs and %d Noisy imgs" % (CleanCount, NoisyCount))
+    organize_pairs(d, n, dng, j)
